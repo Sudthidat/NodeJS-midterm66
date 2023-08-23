bb49a9a6ad5bae6c191e30c2b1ace7b81e588391
Sudthidat <sudthidat64@nu.ac.th>
2023-08-23 13:55:58 +0700

    page6

---
+++ server/src/controllers/que1.js
@@ -0,0 +1,13 @@
+const Model = require('../models/Model');
+
+const que1Controller = (req, res) => {
+    const { number } = req.body;
+    const que2 =  Model. deletedelete(GCD(X,Y));
+   
+    res.json({ que2 });
+};
+
+
+
+module.exports = { que1Controller };
+
--- server/src/controllers/que2.js
+++ server/src/controllers/que2.js
@@ -2,7 +2,7 @@ const Model = require('../models/Model');
 
 const que1Controller = (req, res) => {
     const { number } = req.body;
-    const que2 =  Model. deletedelete(number);
+    const que2 =  Model. deletedelete(X%2);
    
     res.json({ que2 });
 };
